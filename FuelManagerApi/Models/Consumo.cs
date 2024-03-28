ae78c1f04e53e01ea729ed97d9c3c4cc55cbc287
Thiago Augusto <taugusto964@gmail.com>
2024-03-28 13:22:58 -0300

    Adição classes de modelo

---
+++ FuelManagerApi/Models/Consumo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FuelManagerApi.Models
+{
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50,ErrorMessage ="A descriçao deve ter no máximo 50 caracteres")]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public decimal Valor {  get; set; }
+        [Required]
+        public TipoCombustivel TipoCombustivel { get; set; }
+
+        [Required]
+        public int VeiculoId {  get; set; }
+        public Veiculo Veiculo { get; set; }
+    }
+    public enum TipoCombustivel
+    {
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
---
+++ FuelManagerApi/Models/Veiculo.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FuelManagerApi.Models
+{
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Marca { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Modelo {  get; set; }
+        [Required]
+        [StringLength(7,ErrorMessage ="Máximo de 7 caracteres!")]
+        public string Placa {  get; set; }
+        [Required]
+        [Range(1900,2024,ErrorMessage ="Ano de fabricação inválido!")]
+        public int AnoFabricacao {  get; set; }
+        [Required]
+        [Range(1900, 2024, ErrorMessage = "Ano do modelo inválido!")]
+        public int AnoModelo {  get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
+
+    }
+}
